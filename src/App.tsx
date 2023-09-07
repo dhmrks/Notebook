d9b5944e3438522913576f1a0ccac2d0000edae9
Dimitris Rakas <32489127+dhmrks@users.noreply.github.com>
2023-09-07 19:35:42 +0300

    Add hook to handle locastarage [ WIP]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useMemo } from 'react'
 
+import { v4 as uuidV4} from 'uuid'
 import { Container } from 'react-bootstrap'
 import { Route, Routes, Navigate } from 'react-router-dom'
 
@@ -7,14 +8,35 @@ import Home from './components/Home'
 import Note from './components/Note'
 import NewNote from './components/NewNote'
 import EditNote from './components/EditNote'
+import useLocalStorage from './hooks/useLocalStorage'
+import { Tag, RawNotes, NoteData } from './types/NoteTypes'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
+    const [notes, setNotes] = useLocalStorage<RawNotes[]>("NOTES", [])
+    const [tags, setTags] = useLocalStorage<Tag[] >("NOTES", [])
+
+    const notesWithTags = useMemo(() => {
+        return notes.map(note => {
+            return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+        })
+    }, [notes, tags])
+
+    const onCreateNote = ({ tags, ...data}: NoteData) => {
+        setNotes(prevNotes => {
+            return [
+                ...prevNotes,
+                { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }
+            ]
+        })
+    }
 
     return (
         <Container className="my-4">
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/new" element={<NewNote />} />
+                <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
                 <Route path="/:id">
                     <Route index element={<Note />} />
                     <Route path="edit" element={<EditNote />} />
